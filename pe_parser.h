0c2e4de446862910c03e8cfa9702547df2ada733
Xea <0xea@autistici.org>
2023-02-10 09:49:17 +0000

    pe machine type

--- pe_parser.h
+++ pe_parser.h
@@ -1,4 +1,23 @@
 static void pe_parser(FILE * fp)
 {
-	return;
+	unsigned short tok;
+	
+	advance(&tok, 2, fp);
+	
+	print_label("Arch");
+	switch (tok)
+	{
+		case 0x014C:
+			printf("x86\n");
+			break;
+		case 0x8664:
+			printf("x86_64\n");
+			break;
+		case 0x01f0:
+			printf("arm\n");
+			break;
+		case 0x01c0:
+			printf("arm thumb-2\n");
+			break;
+	}
 }
