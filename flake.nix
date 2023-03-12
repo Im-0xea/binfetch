3e467242c394688085bbe7140a6dd6c96706a1dc
Quantenzitrone <quantenzitrone@protonmail.com>
2023-03-12 04:58:39 +0100

    moved source to .src, created nix flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A flake to build binfetch.";
+
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      # Executed by `nix build`
+      packages = rec {
+        binfetch = pkgs.stdenv.mkDerivation {
+          name = "binfetch";
+          src = ./src;
+          buildInputs = with pkgs; [zlib openssl];
+          # TODO make custom config possible
+          # optimally by loading $XDG_CONFIG_HOME ($HOME/.config as fallback)
+          # and $XDG_CONFIG_DIRS (/etc/xdg as fallback)
+          # see https://specifications.freedesktop.org/basedir-spec/latest/ar01s03.html
+          preBuild = "mkdir -p $out/bin $out/etc/binfetch";
+          makeFlags = ["bootstrap" "CONFIG=$(out)/etc/binfetch" "PREFIX=$(out)"];
+          postBuild = "cp cfg/binfetch.cfg $out/etc/binfetch";
+        };
+        default = binfetch;
+      };
+    });
+}
